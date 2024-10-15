5bd84f5dcfedfa81edd2650612703214cfb87154
Victor Herrera <victocode007@gmail.com>
2024-10-14 22:24:20 -0300

    aplicado el slide de marcas y de locutores

--- script.js
+++ script.js
@@ -40,9 +40,21 @@ function stopAutoplay() {
 }
 
 // Iniciar autoplay con un intervalo de 3 segundos.
-startAutoplay(30000);
+startAutoplay(3000);
 
 // Opcional: Detener autoplay cuando el usuario interactúa con los botones de navegación.
 // document.querySelectorAll('.nav-button').forEach(button => {
 //     button.addEventListener('click', stopAutoplay);
 // });
+
+
+//  ACÁ VA SLIDER DE MARCAS
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
